024b30fcebef21ce7aef5e1462dd73b68193537d
Erik Caldwell Jetson <erikcaell@gmail.com>
2024-11-22 19:21:22 -0500

    Config files and launch files for ZED2i camera

---
+++ launch/rviz.launch.py
@@ -0,0 +1,22 @@
+import os
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+    share_dir = get_package_share_directory('elevation_mapping')        
+    return launch.LaunchDescription(
+        [
+            launch_ros.actions.Node(
+                package= 'rviz2',
+                executable= 'rviz2',
+                name= 'rviz',
+                arguments= ['--display-config', os.path.join(share_dir, 'rviz2', 'custom_rviz2.rviz')],
+                output= 'screen'
+            )
+        ]
+    )

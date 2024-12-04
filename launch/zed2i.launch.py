1401cd2a6346ec45391ab9f08bc91faad936889f
Erik Caldwell Jetson <erikcaell@gmail.com>
2024-12-03 19:15:44 -0500

    Create ZED2i example configs and launch

---
+++ launch/zed2i.launch.py
@@ -0,0 +1,38 @@
+import os
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+    share_dir = get_package_share_directory('elevation_mapping')
+    config_dir = os.path.join(share_dir, 'config')
+    list_params = []
+    list_files = [
+        "robots/zed2i_robot.yaml",
+        "elevation_maps/zed2i_map.yaml",
+        "sensor_processors/zed2i_processor.yaml",
+        "postprocessing/postprocessor_pipeline.yaml",
+    ]
+    for file in list_files:
+        if not os.path.isfile(os.path.join(config_dir, file)):
+            raise FileNotFoundError("File not found: " + os.path.join(config_dir, file))
+        list_params.append(os.path.join(config_dir, file))
+    
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print(list_params)
+
+    return launch.LaunchDescription(
+        [
+            launch_ros.actions.Node(
+                package='elevation_mapping',
+                executable='elevation_mapping',
+                name='elevation_mapping',
+                output='screen',
+                parameters=list_params,
+            ),
+        ]
+    )
